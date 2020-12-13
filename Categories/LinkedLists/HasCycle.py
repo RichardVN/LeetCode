178737bd7985dac86171212b3b627f9a482d4d62
RichardVN <59857903+RichardVN@users.noreply.github.com>
2020-12-12 20:19:25 -0800

    Complexity notes for HasCycle

--- Categories/LinkedLists/HasCycle.py
+++ Categories/LinkedLists/HasCycle.py
@@ -1,4 +1,12 @@
 # https://leetcode.com/problems/linked-list-cycle/
+"""
+Time complexity:
+    - No Cycle: O(N) fast pointer reaches end
+    - Cycle: O(N + K) where K is cyclic length, reduce O(N)
+        - N steps for slow to enter cycle, and K iterations for fast to catch slow
+        - NOTE: if fast behind slow:  Number loops to catch = distance between pointers / difference of speed
+    - Space: O(1) , use constant space
+"""
 
 # Definition for singly-linked list.
 # class ListNode:
