c3fee920a82ff20f3a0becc00b0011b779628ebf
RichardVN <59857903+RichardVN@users.noreply.github.com>
2020-12-14 14:24:55 -0800

    add remove nth node from end

---
+++ Categories/LinkedLists/LinkedListFavorites/RemoveNFromEnd.py
@@ -0,0 +1,34 @@
+# https: // leetcode.com/problems/remove-nth-node-from-end-of-list/
+"""
+Time: O(N) to get size, O(N) to remove element
+Space: O(1)
+NOTE: you can two ptrs, that are n nodes apart. Advance both while maintaining n gap.
+        Remove the node at slow upon reaching end. This is one pass.
+"""
+# GIVEN:
+# list is n elements, in range [1, 30]
+# values can only be [0, 100]
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        # First Pass: get size
+        size = 0
+        current = head
+        while current:
+            size += 1
+            current = current.next
+
+        # Second Pass: remove at index
+        # remove head:
+        if size == n:
+            head = head.next
+        # remove another node:
+        else:
+            idx_remove = size - n
+            # find node before removal
+            current = head
+            for i in range(idx_remove - 1):
+                current = current.next
+            # current now at before node. Skip the node to remove
+            current.next = current.next.next
+
+        return head
