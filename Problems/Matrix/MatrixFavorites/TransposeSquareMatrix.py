c4d5a8c925a24d5a1ead99f067cf7f019d90026e
RichardVN <59857903+RichardVN@users.noreply.github.com>
2021-01-26 13:06:52 -0800

    transpose example

--- Problems/Matrix/MatrixFavorites/TransposeSquareMatrix.py
+++ Problems/Matrix/MatrixFavorites/TransposeSquareMatrix.py
@@ -4,3 +4,13 @@ def transpose_matrix(matrix):
         # TODO: start at column i, or else we just end up with original array
         for j in range(i, N):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+if __name__ == "__main__":
+    m = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ]
+    transpose_matrix(m)
+    for row in m:
+        print(row)
