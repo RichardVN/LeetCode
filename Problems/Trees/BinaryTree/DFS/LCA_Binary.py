5681f2e72202d25603ecba0c3a9ed09307e9b80c
Richard Nguyen <nguyen.richard.v@gmail.com>
2023-07-07 11:24:32 -0700

    q with two stacks

--- Problems/Trees/BinaryTree/DFS/LCA_Binary.py
+++ Problems/Trees/BinaryTree/DFS/LCA_Binary.py
@@ -22,26 +22,24 @@ Space: O(N)
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # dfs helper .. return UP  =>  (p_found, q_found)
-        def dfs(root, p, q):
-            # base 
-            if not root: 
+        def dfs(root):
+            if not root:
                 return (False, False)
-            # p_found iff ... at p, or p is in either subtree (recursive dfs)
-            p_left, q_left = dfs(root.left, p, q)
-            p_right, q_right = dfs(root.right, p, q)
+            pfoundleft, qfoundleft = dfs(root.left)
+            pfoundright, qfoundright = dfs(root.right)
 
-            p_found = (root is p) or p_left or p_right
-            q_found = (root is q) or q_left or q_right
+            # boolean direct assignment : p_found iff ... root at p, or p is in either subtree (recursive dfs)
+            pfound = root == p or pfoundleft or pfoundright
+            qfound =  root == q or qfoundleft or qfoundright
 
-            # TODO: do we have both? If lca not found yet, set as root
             nonlocal lca
-            if p_found and q_found and lca is None:
+            # TODO: do we have both? If lca not found yet, set as root
+            if lca is None and pfound and qfound:
                 lca = root
 
-            return (p_found, q_found)
-        
-        # LCA found at any point
+            return (pfound, qfound)
+
+        # global var cuz answer can occur at any node in tree
         lca = None
-        dfs(root, p, q)
+        dfs(root)
         return lca
